3d7cf779187bed410b55dbb134b8ac101e3a4dde
krescmj <30279119+krescmj@users.noreply.github.com>
2018-03-27 16:03:22 -0500

    Update TablePartE.py
    dsafsad

--- TablePartE.py
+++ TablePartE.py
@@ -1,12 +1,8 @@
 import happybase as hb
 
-import happybase as hb
-
 conn = hb.Connection()
 
 table = conn.table('powers')
-
-#columns = (b'personal:name', b'personal:city')
 
 for key, data in table.scan(include_timestamp=True):
     print('Found: {}, {}'.format(key, data))
